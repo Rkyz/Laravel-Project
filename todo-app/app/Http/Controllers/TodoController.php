29353b45956eb94d3dc07ce3992b0e01f0c5a904
Rkyz <rioalamsyah@smkwikrama.sch.id>
2022-11-10 11:18:37 +0700

    -

--- todo-app/app/Http/Controllers/TodoController.php
+++ todo-app/app/Http/Controllers/TodoController.php
@@ -15,7 +15,7 @@ class TodoController extends Controller
     public function index()
     {
         //untuk menampilkan halaman awal & semua data
-        return view('login');
+        return view('layout');
     }
 
     /**
